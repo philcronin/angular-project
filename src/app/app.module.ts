52eca3c629f881638f7f27d4f2e6880f21a93899
Clint Langwell <clintlangwell@gmail.com>
2020-11-16 09:13:04 -0500

    added trending carousel

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,18 @@
+import { HttpClient } from '@angular/common/http';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+
+import { Swiper } from 'swiper/bundle';
+import 'swiper/swiper-bundle.css';
+import { SwiperModule } from 'ngx-swiper-wrapper';
+import { SWIPER_CONFIG } from 'ngx-swiper-wrapper';
+import { SwiperConfigInterface } from 'ngx-swiper-wrapper';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { CarouselModule } from 'ngx-owl-carousel-o';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { TrendingComponent } from './trending/trending.component';
@@ -12,6 +22,13 @@ import { SearchComponent } from './search/search.component';
 import { MovieInfoComponent } from './movie-info/movie-info.component';
 import { WatchListComponent } from './watch-list/watch-list.component';
 import { HomeComponent } from './home/home.component';
+
+const swiper = new Swiper('.swiper-container');
+
+const DEFAULT_SWIPER_CONFIG: SwiperConfigInterface = {
+  direction: 'horizontal',
+  slidesPerView: 'auto',
+};
 
 @NgModule({
   declarations: [
@@ -24,13 +41,22 @@ import { HomeComponent } from './home/home.component';
     SearchComponent,
     MovieInfoComponent,
     WatchListComponent,
-    HomeComponent
+    HomeComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    // CarouselModule,
+    SwiperModule,
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [
+    {
+      provide: SWIPER_CONFIG,
+      useValue: DEFAULT_SWIPER_CONFIG,
+    },
+  ],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/movie.service.ts
+++ src/app/movie.service.ts
@@ -1,9 +1,30 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class MovieService {
+  trendingBaseUrl: string = 'https://api.themoviedb.org/3/trending/movie/week';
+  key: string = '4d6b6d705d0ccdf5166bc895195797a2';
+  mediaType = 'movie';
+  timeWindow = 'week';
+  genreUrl = 'https://api.themoviedb.org/3/genre/movie/list';
+  constructor(private http: HttpClient) {}
+  getTrending = () => {
+    return this.http.get(this.trendingBaseUrl, {
+      params: {
+        api_key: this.key,
+        media_type: this.mediaType,
+        time_window: this.timeWindow,
+      },
+    });
+  };
 
-  constructor() { }
+  getGenreList = () => {
+    return this.http.get(this.genreUrl, {
+      params: {
+        api_key: this.key,
+      },
+    });
+  };
 }
