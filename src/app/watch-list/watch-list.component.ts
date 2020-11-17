e07e95efdd3ebdb5512003062b47c2653b45bd28
Rachel Polant <ruchelah@gmail.com>
2020-11-17 17:42:19 -0500

    added favorites to watch list and removed them

--- src/app/watch-list/watch-list.component.ts
+++ src/app/watch-list/watch-list.component.ts
@@ -1,15 +1,30 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { Movie } from '../interfaces/movie';
+import { MovieService } from '../movie.service';
 
 @Component({
   selector: 'app-watch-list',
   templateUrl: './watch-list.component.html',
-  styleUrls: ['./watch-list.component.css']
+  styleUrls: ['./watch-list.component.css'],
 })
 export class WatchListComponent implements OnInit {
+  watchList: Movie[] = [];
+  @Output() watchListEvent = new EventEmitter<Movie>();
 
-  constructor() { }
+  constructor(private movieService: MovieService) {}
 
   ngOnInit(): void {
+    this.watchList = this.movieService.getWatchList();
+    console.log(this.watchList);
   }
 
+  sendToWatchList = (movie: any) => {
+    let newMovie: Movie = {
+      title: movie.title,
+      genre: movie.genre,
+      image: movie.image,
+      id: movie.id,
+    };
+    this.movieService.addToWatchList(newMovie);
+  };
 }
